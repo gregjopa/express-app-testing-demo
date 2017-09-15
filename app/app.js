f8d4619da35ad25d7ce70509e16f8d23cd95f57b
Greg Jopa <grjopa@gmail.com>
2017-09-14 19:39:02 -0500

    Add test coverage with jest

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ var photoModel = require('./photo_model');
 // public assets
 app.use(express.static(__dirname + '/public'));
 app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use('/coverage', express.static(__dirname + '/../test/coverage/reports'));
+app.use('/coverage', express.static(__dirname + '/../coverage'));
 
 
 
