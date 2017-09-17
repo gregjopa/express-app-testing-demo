61c935614f6ca3394a07b70258789533052c963b
Greg Jopa <grjopa@gmail.com>
2017-09-17 08:08:38 -0500

    Remove redundant 'describe' blocks

--- test/unit/jsonp_helper.test.js
+++ test/unit/jsonp_helper.test.js
@@ -1,32 +1,30 @@
 const jsonpHelper = require('../../app/jsonp_helper');
 
-describe('jsonp_helper.js', () => {
-  describe('parseJSONP(jsonpData)', () => {
-    test('should parse valid jsonp data', () => {
-      const jsonpData = 'jsonFlickrFeed({"title": "tagged california"});';
-      const jsObject = jsonpHelper.parseJSONP(jsonpData);
-      expect(jsObject).toMatchObject({
-        title: 'tagged california'
-      });
+describe('parseJSONP(jsonpData)', () => {
+  test('should parse valid jsonp data', () => {
+    const jsonpData = 'jsonFlickrFeed({"title": "tagged california"});';
+    const jsObject = jsonpHelper.parseJSONP(jsonpData);
+    expect(jsObject).toMatchObject({
+      title: 'tagged california'
     });
+  });
 
-    test('should parse jsonp data with escaped single quotes', () => {
-      const jsonpData =
-        'jsonFlickrFeed({"title": "tagged california\'s coast"});';
-      const jsObject = jsonpHelper.parseJSONP(jsonpData);
-      expect(jsObject).toMatchObject({
-        title: "tagged california's coast"
-      });
+  test('should parse jsonp data with escaped single quotes', () => {
+    const jsonpData =
+      'jsonFlickrFeed({"title": "tagged california\'s coast"});';
+    const jsObject = jsonpHelper.parseJSONP(jsonpData);
+    expect(jsObject).toMatchObject({
+      title: "tagged california's coast"
     });
+  });
 
-    test('should throw error when parsing invalid jsonp data', () => {
-      // invalid json because of missing double quotes around title value
-      const jsonpData = 'jsonFlickrFeed({"title": tagged california});';
+  test('should throw error when parsing invalid jsonp data', () => {
+    // invalid json because of missing double quotes around title value
+    const jsonpData = 'jsonFlickrFeed({"title": tagged california});';
 
-      expect(function() {
-        // call the add(item) method without passing in an item
-        jsonpHelper.parseJSONP(jsonpData);
-      }).toThrowError(/Failed to convert jsonp to json/);
-    });
+    expect(function() {
+      // call the add(item) method without passing in an item
+      jsonpHelper.parseJSONP(jsonpData);
+    }).toThrowError(/Failed to convert jsonp to json/);
   });
 });
