727f9e967a6c80450baa97b13a047ad4030918a5
Greg Jopa <gjopa@cars.com>
2017-09-20 10:42:50 -0500

    Update supertest to use promises

--- app/__tests__/app.test.js
+++ app/__tests__/app.test.js
@@ -8,32 +8,44 @@ describe('index route', () => {
     app.server.close();
   });
 
-  test('should respond with a 200 with no query parameters', done => {
-    request(app)
+  test('should respond with a 200 with no query parameters', () => {
+    return request(app)
       .get('/')
       .expect('Content-Type', /html/)
-      .expect(200, done);
+      .expect(200)
+      .then(response => {
+        expect(response.text).toMatch(/<title>Express App Testing Demo<\/title>/);
+      });
   });
 
-  test('should respond with a 200 with valid query parameters', done => {
-    request(app)
+  test('should respond with a 200 with valid query parameters', () => {
+    return request(app)
       .get('/?tags=california&tagmode=all')
       .expect('Content-Type', /html/)
       .expect(200)
-      .expect(/<div class="panel panel-default search-results">/, done);
+      .then(response => {
+        expect(response.text).toMatch(/<div class="panel panel-default search-results">/);
+      });
+
   });
 
-  test('should respond with a 200 with invalid query parameters', done => {
-    request(app)
+  test('should respond with a 200 with invalid query parameters', () => {
+    return request(app)
       .get('/?tags=california123&tagmode=all')
       .expect('Content-Type', /html/)
       .expect(200)
-      .expect(/<div class="alert alert-danger">/, done);
+      .then(response => {
+        expect(response.text).toMatch(/<div class="alert alert-danger">/);
+      });
   });
 
-  test('should respond with a 500 error due to bad jsonp data', done => {
-    request(app)
+  test('should respond with a 500 error due to bad jsonp data', () => {
+    return request(app)
       .get('/?tags=error&tagmode=all')
-      .expect(500, done);
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .then(response => {
+        expect(response.body).toEqual({ error: 'Internal server error' });
+      });
   });
 });
