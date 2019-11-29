5412b93b75e6bc534de00f2c76fda152e1beab58
venkatesh19889 <56393977+venkatesh19889@users.noreply.github.com>
2019-11-29 12:50:31 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=sample-project \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://softosonar.symple.co.in \
+  -Dsonar.login=186033caf02975d4f7bc1b99964c2e87581fbffb
